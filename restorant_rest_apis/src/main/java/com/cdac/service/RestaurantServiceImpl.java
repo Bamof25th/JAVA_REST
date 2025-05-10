a75494e988b67843c36f61826467b7e7fa1df2f4
Aniket Baghel <88363603+Bamof25th@users.noreply.github.com>
2025-05-10 12:32:02 +0530

    in Class produt rest apis 2

---
+++ restorant_rest_apis/src/main/java/com/cdac/service/RestaurantServiceImpl.java
@@ -0,0 +1,70 @@
+package com.cdac.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.cdac.custom_exceptions.ApiException;
+import com.cdac.custom_exceptions.ResourceNotFoundException;
+import com.cdac.dao.RestaurantDao;
+import com.cdac.entities.Restaurant;
+
+@Service // => spring bean containing B.L
+@Transactional // => auto tx management
+public class RestaurantServiceImpl implements RestaurantService {
+
+	@Autowired
+	private RestaurantDao restaurantDao;
+
+	@Override
+	public List<Restaurant> getAllRestaurants() {
+		return restaurantDao.findAll();
+	}
+
+	@Override
+	public Restaurant getRestaurantDetails(Long restaurantId) {
+
+		Restaurant restaurant = restaurantDao.findById(restaurantId)
+				.orElseThrow(
+						() -> new ResourceNotFoundException("Invalid Restaurant ID - not found !!!!!"));
+		// => valid id , restaurant : persistent
+		// simply access the size of the collection
+		// restaurant.getFoodItems().size();//triggers another select query - food_items
+		return restaurant;
+	}
+
+	@Override
+	public Restaurant addNewRestaurant(Restaurant transientEntity) {
+		/*
+		 * validate if the restaurant name already exists -yes - throw custom exception
+		 * - name alrdy exists - no continue
+		 */
+		if (restaurantDao.findByName(
+				transientEntity.getName()).isEmpty())
+			return restaurantDao.save(transientEntity);
+		throw new ApiException("Dup restaurant name !!!!!!");
+	} // when transactional method rets - tx over - tx .commit
+		// service layer rets DETACHED entity to the caller
+
+	@Override
+	public Restaurant updateRestaurant(Long rid, Restaurant newRes) {
+		getRestaurantDetails(rid);
+		if (restaurantDao.findByName(
+				newRes.getName()).isEmpty()) {
+			return restaurantDao.save(newRes);
+		} else {
+			throw new ApiException("Dup restaurant name !!!!!!");
+		}
+	}
+
+	@Override
+	public String deleteRestaurant(String name) {
+		Restaurant rest = restaurantDao.findByName(name)
+				.orElseThrow(() -> new ApiException("Dup restaurant name !!!!!!"));
+		restaurantDao.delete(rest);
+		return "Deleted Successfully";
+	}
+
+}
